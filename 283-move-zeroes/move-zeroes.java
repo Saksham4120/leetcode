398c11bb65e8c72ae56ca3165b0447aceaa24f2f
Saksham Sharma <132200218+Saksham4120@users.noreply.github.com>
2024-06-10 15:31:01 +0530

    Time: 2 ms (83.95%) | Memory: 46.1 MB (27.44%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,26 +1,38 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        int j = -1;
+//         int n = nums.length;
+//         int j = -1;
 
-//step 1
-    for(int i=0; i<n; i++){
-        if(nums[i] == 0){
-            j = i;
-            break;
+// //step 1
+//     for(int i=0; i<n; i++){
+//         if(nums[i] == 0){
+//             j = i;
+//             break;
+//         }
+//     }
+//     if(j == -1){
+//         return ;
+//     }
+
+//         for(int i=j+1; i<n; i++){
+//             if(nums[i] != 0 ){
+//                int swap = nums[j] ;
+//                nums [j] = nums[i];
+//                nums[i] = swap;
+//                j++;
+//             }
+//         }
+//     }
+
+int j = 0;
+    for(int i = 0; i < nums.length; i++) {
+        if(nums[i] != 0) {
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+            j++;
         }
     }
-    if(j == -1){
-        return ;
-    }
 
-        for(int i=j+1; i<n; i++){
-            if(nums[i] != 0 ){
-               int swap = nums[j] ;
-               nums [j] = nums[i];
-               nums[i] = swap;
-               j++;
-            }
-        }
     }
 }
